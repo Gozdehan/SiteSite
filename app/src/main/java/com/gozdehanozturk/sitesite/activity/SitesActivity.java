7652db3496d649da47d37865779d2872633329cc
Gozdehan <gozdehanozturk@gmail.com>
2017-10-31 21:28:22 +0300

    sitesactivity ye menu eklendi

--- app/src/main/java/com/gozdehanozturk/sitesite/activity/SitesActivity.java
+++ app/src/main/java/com/gozdehanozturk/sitesite/activity/SitesActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -39,11 +41,27 @@ public class SitesActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
            case android.R.id.home:
                finish();
                return true;
+            case R.id.fav:
+                startActivity(new Intent(SitesActivity.this,FavoriteSitesActivity.class));
+                return true;
+            case R.id.search:
+                startActivity(new Intent(SitesActivity.this,SearchActivity.class));
+                return true;
+            case R.id.offer:
+                startActivity(new Intent(SitesActivity.this,OfferActivity.class));
+                return true;
         }
         return super.onOptionsItemSelected(item);
     }
